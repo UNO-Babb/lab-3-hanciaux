002f428e7cf1062886b5e3ea668372b302b167d4
hanciaux <hanciaux@unomaha.edu>
2025-02-07 16:46:11 +0000

    finished rps

--- RPS.py
+++ RPS.py
@@ -9,74 +9,69 @@ def main():
   ties = 0
   losses = 0
 
-  wins = int(wins)
-  wins = int(ties)
-  losses = int(losses)
-
   #Create a loop that continues as long as the user wants to play.
   #User can play as many games as they wish.
+  play = "Y"
+  while (play == "Y"):
+    #Randomly choose the computer between 'R', 'P', or 'S'
+    CPUchoice = random.choice(["R", "P", "S"])
 
-  #Randomly choose the computer between 'R', 'P', or 'S'
-  CPUchoice = random.choice(["R", "P", "S"])
+    #Prompt the user for their RPS selection
 
-  #Prompt the user for their RPS selection
+    userChoice = input("Choose Rock, Paper, or Scissors (R, P, S): ")
 
-  userChoice = input("Choose Rock, Paper, or Scissors (R, P, S): ")
+    #Determine winner and state what happened to the user
 
-  #Determine winner and state what happened to the user
+    if userChoice == "R":
+      print("You chose Rock")
+      if CPUchoice == "R":
+        print("The computer chose Rock")
+        print("Tie")
+        ties = ties + 1
+      if CPUchoice == "P":
+        print("The computer chose Paper")
+        print("LOSER!")
+        losses = losses + 1
+      if CPUchoice == "S":
+        print("The computer chose Scissors")
+        print("You Win!")
+        wins = wins + 1
 
-  if userChoice == "R":
-    print("You chose Rock")
-    if CPUchoice == "R":
-      print("The computer chose Rock")
-      print("Tie")
-      ties = ties + 1
-    if CPUchoice == "P":
-      print("The computer chose Paper")
-      print("LOSER!")
-      losses = losses + 1
-    if CPUchoice == "S":
-      print("The computer chose Scissors")
-      print("You Win!")
-      wins = wins + 1
+    if userChoice == "P":
+      print("You chose Paper")
+      if CPUchoice == "P":
+        print("The computer chose Paper")
+        print("TIE")
+        ties = ties + 1
+      if CPUchoice == "S":
+        print("The computer chose Scissors")
+        print("LOSER!")
+        losses = losses + 1
+      if CPUchoice == "R":
+        print("The computer chose Rock")
+        print("You Win!")
+        wins = wins + 1
 
-  if userChoice == "P":
-    print("You chose Paper")
-    if CPUchoice == "P":
-      print("The computer chose Paper")
-      print("TIE")
-      ties = ties + 1
-    if CPUchoice == "S":
-      print("The computer chose Scissors")
-      print("LOSER!")
-      losses = losses + 1
-    if CPUchoice == "R":
-      print("The computer chose Rock")
-      print("You Win!")
-      wins = wins + 1
+    if userChoice == "S":
+      print("You chose Scissors")
+      if CPUchoice == "S":
+        print("The computer chose Scissors")
+        print("TIE")
+        ties = ties + 1
+      if CPUchoice == "R":
+        print("The computer chose Rock")
+        print("LOSER!")
+        losses = losses + 1
+      if CPUchoice == "P":
+        print("The computer chose Paper")
+        print("You Win!")
+        wins = wins + 1
 
-  if userChoice == "S":
-    print("You chose Scissors")
-    if CPUchoice == "S":
-      print("The computer chose Scissors")
-      print("TIE")
-      ties = ties + 1
-    if CPUchoice == "R":
-      print("The computer chose Rock")
-      print("LOSER!")
-      losses = losses + 1
-    if CPUchoice == "P":
-      print("The computer chose Paper")
-      print("You Win!")
-      wins = wins + 1
-
-
-  
-
-  #Ask the user if they would like to play again.
-  play = input("Play again (Y/N)? ")
-  if play == "Y":
+    #Ask the user if they would like to play again.
+    play = input("Play again (Y/N)? ")
     
+  print("Game over")
+      
 
 
   #In the end, print the stats
